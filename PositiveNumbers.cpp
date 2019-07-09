9342f5075553a11ec4996b08693dcc2a9bc3d34a
suhadev <suhadevvenkatesh@gmail.com>
2019-07-09 16:09:21 +0530

    solve positivenumbers

--- PositiveNumbers.cpp
+++ PositiveNumbers.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
 using namespace std;
-string positiveNumbers(int num)
-{
 
+string main()
+{
+  int num;
+    cin>>num;
     if (num > 0)
     {
-        cout << "Positive";
         return "Positive";
     }
     else if (num < 0)
     {
-        cout << "Negative";
         return "negative";
     } else{
         return "zero";
     }
-}
-int main()
-{
-    positiveNumbers(-5);
 }
