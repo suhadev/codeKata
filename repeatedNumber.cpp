ad132c5d9480e31d7b6ac94448555c98ce135a56
suhadev <suhadevvenkatesh@gmail.com>
2019-07-09 18:35:08 +0530

    solve duplicate elements in array

---
+++ repeatedNumber.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++) {
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++){
+    	for(int j=i+1;j<n;j++){
+    		if(a[i] == a[j]){
+    			cout<<"\n"<<a[i]<<" ";
+    		}
+    	}
+    
+    }
+    
+    
+}
