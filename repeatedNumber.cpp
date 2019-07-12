db4a206f67b44a06be73b02658cf8cbd5e261d46
suhadev <suhadevvenkatesh@gmail.com>
2019-07-12 21:16:09 +0530

    Remove commented lines

--- repeatedNumber.cpp
+++ repeatedNumber.cpp
@@ -1,21 +1,3 @@
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cin>>n;
-//     int a[n];
-//     for(int i=0;i<n;i++) {
-//         cin>>a[i];
-//     }
-//     for(int i=0;i<n;i++){
-//     	for(int j=i+1;j<n;j++){
-//     		if(a[i] == a[j]){
-//     			cout<<a[i]<<" ";
-//     		}
-//     	}
-//     }   
-// }
-
 #include<iostream>
 #include<bits/stdc++.h>
 #include<array>
