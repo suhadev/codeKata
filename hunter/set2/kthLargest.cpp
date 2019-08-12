2e4196a8930c1d5ac4b8970fa679e3f3fcddde28
suhadev <suhadevvenkatesh@gmail.com>
2019-08-12 13:40:33 +0530

    solve

--- hunter/set2/kthLargest.cpp
+++ hunter/set2/kthLargest.cpp
@@ -4,17 +4,13 @@ int temp;
 int main(){
 	int n,k;
 	cin>>n>>k;
-	int a[n];
+	int a[100];
 	for(int i=0;i<n;i++){
 		cin>>a[i];
 	}
 	for(int i=0;i<n;i++){
-		//int el = arr[i];
-		//cout<<"ele"<<a[i]<<endl;
 		for(int j=i+1;j<5;j++){
 			if(a[i] > a[j]){
-				//cout<<a[i]<<" greater than "<<a[j]<<endl;
-				//cout<<"moving"<<a[i]<<"from"<<i<<"to"<<j<<endl;
 				temp = a[j];
 				a[j] = a[i];
 				a[i] = temp;
